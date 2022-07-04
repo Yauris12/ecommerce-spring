9294c9d31f4d4e21df4e158a3a4f6d0c1425280e
Yauris12 <alexisaube28@gmail.com>
2022-07-04 08:15:06 -0500

    create repository sercice  method show productControoler

---
+++ src/main/java/com/ecommerce/app/repository/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.app.repository;
+
+import com.ecommerce.app.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+
+public interface ProductoRepository  extends JpaRepository<Producto,Integer> {
+}
